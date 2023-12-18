5ffcd529c523550fdb2de41737dee258157164f7
Edward Magafurov <edward801@yandex.ru>
2023-12-18 17:12:06 +0500

    add Lsn and Task

---
+++ Task2/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход 
+//число и проверяет, кратно ли оно одновременно 7 и 23.
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32 (Console.ReadLine());
+
+if (number % 23 == 0)
+    {
+        if (number % 7 == 0)
+        {
+            Console.WriteLine("Число кратно 23 и 7");
+        }
+        else
+        {
+            Console.WriteLine("Число не кратно 7");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Число не кратно 23");
+    }
