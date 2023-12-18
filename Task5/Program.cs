6450380b1843f5dddb5fc6b083682419043d4c97
Edward Magafurov <edward801@yandex.ru>
2023-12-18 19:34:30 +0500

    Add Task5

---
+++ Task5/Program.cs
@@ -0,0 +1,10 @@
+﻿//Напишите программу, которая на вход принимает натуральное число N, 
+//а на выходе показывает его цифры через запятую.
+
+Console.WriteLine("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int firstDigit = number / 10;
+int secondDigit = number % 10;
+Console.WriteLine($"цифры {firstDigit},{secondDigit}");
+
+
